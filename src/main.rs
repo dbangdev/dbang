dc25c1b0df8eeeda1e1ad1261ab53fe054a9af53
linux_china <libing.chen@gmail.com>
2021-12-31 12:42:58 -0800

    Implement simple run command

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 mod app;
+mod deno_cli;
+mod catalog;
 
 use crate::app::build_app;
 use colored::*;
+use std::env;
 
 fn main() {
     let app = build_app();
@@ -12,5 +15,19 @@ fn main() {
             "😂 Please use subcommand or --help to display help!".red()
         );
         return;
+    }
+    let (sub_command, args) = matches.subcommand().unwrap();
+    if sub_command == "run" {
+        let artifact_name = args.value_of("artifact").unwrap();
+        println!("{}", artifact_name);
+        let mut artifact_args = vec![];
+        if let Some(params) = args.values_of("params") {
+            artifact_args = params.collect::<Vec<&str>>()
+        }
+        let mut permissions = vec![];
+        deno_cli::run("https://raw.githubusercontent.com/linux-china/dbang-catalog/main/hello.ts",
+                      &artifact_args,
+                      &permissions,
+        );
     }
 }
