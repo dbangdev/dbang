3df031aeb6d8fd1fc2813c5f8b9d3619882122f5
linux_china <libing.chen@gmail.com>
2022-01-01 14:57:57 -0800

    Adjust structure

--- src/catalog.rs
+++ src/catalog.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 use reqwest::blocking::Client;
 use std::path::{Path, PathBuf};
 use std::{fs};
+use crate::deno_cli;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Catalog {
@@ -19,6 +20,12 @@ pub struct Artifact {
 }
 
 impl Artifact {
+    pub fn read_from_local(github_user: &str, artifact_name: &str) -> anyhow::Result<Artifact> {
+        let catalog = Catalog::read_from_local(github_user)?;
+        let artifact = catalog.scripts.get(artifact_name).unwrap();
+        Ok(artifact.clone())
+    }
+
     pub fn get_script_http_url(&self, github_user: &str) -> String {
         return if self.script_ref.starts_with("https://") || self.script_ref.starts_with("http://") {
             self.script_ref.to_string()
@@ -43,19 +50,47 @@ impl Artifact {
     }
 }
 
-pub fn get_artifact(github_user: &str, artifact_name: &str) -> anyhow::Result<Artifact> {
-    let catalog = read_local_nbang_catalog(github_user)?;
-    let artifact = catalog.scripts.get(artifact_name).unwrap();
-    Ok(artifact.clone())
+impl Catalog {
+    pub fn cache_artifacts(&self, github_user: &str) -> anyhow::Result<()> {
+        for (_k, v) in self.scripts.iter() {
+            deno_cli::cache(&v.get_script_http_url(github_user))?;
+        };
+        Ok(())
+    }
+
+    pub fn fetch_from_github(github_user: &str) -> anyhow::Result<Catalog> {
+        let url = format!("https://raw.githubusercontent.com/{}/dbang-catalog/main/dbang-catalog.json", github_user);
+        let client = Client::new();
+        let response = client.get(&url).send()?;
+        let catalog: Catalog = response.json()?;
+        Ok(catalog)
+    }
+
+    pub fn read_from_local(github_user: &str) -> anyhow::Result<Catalog> {
+        let home_dir: PathBuf = dirs::home_dir().unwrap();
+        let dbang_catalog_json_file = Path::new(&home_dir)
+            .join(".dbang")
+            .join("catalogs/github")
+            .join(github_user)
+            .join("dbang-catalog")
+            .join("dbang-catalog.json");
+        let data = fs::read_to_string(dbang_catalog_json_file).expect("Unable to read dbang-catalog.json");
+        let catalog: Catalog = serde_json::from_str(&data).expect("Unable to parse dbang-catalog.json");
+        Ok(catalog)
+    }
+
+    pub fn local_exists(github_user: &str) -> anyhow::Result<bool> {
+        let home_dir: PathBuf = dirs::home_dir().unwrap();
+        let dbang_catalog_json_file = Path::new(&home_dir)
+            .join(".dbang")
+            .join("catalogs/github")
+            .join(github_user)
+            .join("dbang-catalog")
+            .join("dbang-catalog.json");
+        Ok(dbang_catalog_json_file.exists())
+    }
 }
 
-pub fn fetch_remote_nbang_catalog(github_user: &str) -> anyhow::Result<Catalog> {
-    let url = format!("https://raw.githubusercontent.com/{}/dbang-catalog/main/dbang-catalog.json", github_user);
-    let client = Client::new();
-    let response = client.get(&url).send()?;
-    let catalog: Catalog = response.json()?;
-    Ok(catalog)
-}
 
 pub fn save_nbang_catalog_from_json(github_user: &str, json_text: &str) -> anyhow::Result<()> {
     let home_dir: PathBuf = dirs::home_dir().unwrap();
@@ -70,7 +105,7 @@ pub fn save_nbang_catalog_from_json(github_user: &str, json_text: &str) -> anyho
     Ok(())
 }
 
-pub fn save_nbang_catalog(github_user: &str) -> anyhow::Result<()> {
+pub fn save_remote_nbang_catalog(github_user: &str) -> anyhow::Result<()> {
     let url = format!("https://raw.githubusercontent.com/{}/dbang-catalog/main/dbang-catalog.json", github_user);
     let response = Client::builder()
         .build()?
@@ -81,29 +116,6 @@ pub fn save_nbang_catalog(github_user: &str) -> anyhow::Result<()> {
     save_nbang_catalog_from_json(github_user, &json_text)
 }
 
-pub fn local_nbang_catalog_exists(github_user: &str) -> anyhow::Result<bool> {
-    let home_dir: PathBuf = dirs::home_dir().unwrap();
-    let dbang_catalog_json_file = Path::new(&home_dir)
-        .join(".dbang")
-        .join("catalogs/github")
-        .join(github_user)
-        .join("dbang-catalog")
-        .join("dbang-catalog.json");
-    Ok(dbang_catalog_json_file.exists())
-}
-
-pub fn read_local_nbang_catalog(github_user: &str) -> anyhow::Result<Catalog> {
-    let home_dir: PathBuf = dirs::home_dir().unwrap();
-    let dbang_catalog_json_file = Path::new(&home_dir)
-        .join(".dbang")
-        .join("catalogs/github")
-        .join(github_user)
-        .join("dbang-catalog")
-        .join("dbang-catalog.json");
-    let data = fs::read_to_string(dbang_catalog_json_file).expect("Unable to read dbang-catalog.json");
-    let catalog: Catalog = serde_json::from_str(&data).expect("Unable to parse dbang-catalog.json");
-    Ok(catalog)
-}
 
 mod tests {
     use super::*;
@@ -135,9 +147,9 @@ mod tests {
     #[test]
     fn test_save_nbang_catalog() -> anyhow::Result<()> {
         let github_user = "linux-china";
-        save_nbang_catalog(github_user)?;
-        if local_nbang_catalog_exists(github_user).is_ok() {
-            let catalog = read_local_nbang_catalog(github_user)?;
+        save_remote_nbang_catalog(github_user)?;
+        if Catalog::local_exists(github_user).is_ok() {
+            let catalog = Catalog::read_from_local(github_user)?;
             println!("catalog = {:?}", catalog);
         }
         Ok(())
@@ -145,21 +157,21 @@ mod tests {
 
     #[test]
     fn test_fetch_remote_nbang_catalog() -> anyhow::Result<()> {
-        let catalog = fetch_remote_nbang_catalog("linux-china")?;
+        let catalog = Catalog::fetch_from_github("linux-china")?;
         println!("catalog = {:?}", catalog);
         Ok(())
     }
 
     #[test]
     fn test_read_local_dbang_catalog() -> anyhow::Result<()> {
-        let catalog = read_local_nbang_catalog("linux-china")?;
+        let catalog = Catalog::read_from_local("linux-china")?;
         println!("catalog = {:?}", catalog);
         Ok(())
     }
 
     #[test]
     fn test_get_artifact() {
-        let artifact = get_artifact("linux-china", "hello").unwrap();
+        let artifact = Artifact::read_from_local("linux-china", "hello").unwrap();
         println!("artifact = {:?}", artifact);
         println!("url = {}", artifact.get_script_http_url("linux-china"));
     }
