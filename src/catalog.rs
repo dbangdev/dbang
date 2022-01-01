0619064d46b7c3c625b07b62b687d55740e74cac
linux_china <libing.chen@gmail.com>
2022-01-01 14:32:42 -0800

    Add APIs for catalog: fetch from remote, save and read from local

--- src/catalog.rs
+++ src/catalog.rs
@@ -1,6 +1,8 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use reqwest::blocking::Client;
+use std::path::{Path, PathBuf};
+use std::{fs};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Catalog {
@@ -41,20 +43,65 @@ impl Artifact {
     }
 }
 
-pub fn get_artifact(github_user: &str, artifact_name: &str) -> reqwest::Result<Artifact> {
-    let catalog = fetch_nbang_catalog(github_user)?;
+pub fn get_artifact(github_user: &str, artifact_name: &str) -> anyhow::Result<Artifact> {
+    let catalog = read_local_nbang_catalog(github_user)?;
     let artifact = catalog.scripts.get(artifact_name).unwrap();
     Ok(artifact.clone())
 }
 
-pub fn fetch_nbang_catalog(github_user: &str) -> reqwest::Result<Catalog> {
+pub fn fetch_remote_nbang_catalog(github_user: &str) -> anyhow::Result<Catalog> {
+    let url = format!("https://raw.githubusercontent.com/{}/dbang-catalog/main/dbang-catalog.json", github_user);
+    let client = Client::new();
+    let response = client.get(&url).send()?;
+    let catalog: Catalog = response.json()?;
+    Ok(catalog)
+}
+
+pub fn save_nbang_catalog_from_json(github_user: &str, json_text: &str) -> anyhow::Result<()> {
+    let home_dir: PathBuf = dirs::home_dir().unwrap();
+    let dbang_catalog_dir = Path::new(&home_dir)
+        .join(".dbang")
+        .join("catalogs/github")
+        .join(github_user)
+        .join("dbang-catalog");
+    std::fs::create_dir_all(&dbang_catalog_dir)?;
+    let dbang_catalog_file = dbang_catalog_dir.join("dbang-catalog.json");
+    std::fs::write(&dbang_catalog_file, json_text)?;
+    Ok(())
+}
+
+pub fn save_nbang_catalog(github_user: &str) -> anyhow::Result<()> {
     let url = format!("https://raw.githubusercontent.com/{}/dbang-catalog/main/dbang-catalog.json", github_user);
     let response = Client::builder()
         .build()?
         .get(&url)
         .header("Accept", "application/json")
         .send()?;
-    let catalog = response.json::<Catalog>()?;
+    let json_text = response.text()?;
+    save_nbang_catalog_from_json(github_user, &json_text)
+}
+
+pub fn local_nbang_catalog_exists(github_user: &str) -> anyhow::Result<bool> {
+    let home_dir: PathBuf = dirs::home_dir().unwrap();
+    let dbang_catalog_json_file = Path::new(&home_dir)
+        .join(".dbang")
+        .join("catalogs/github")
+        .join(github_user)
+        .join("dbang-catalog")
+        .join("dbang-catalog.json");
+    Ok(dbang_catalog_json_file.exists())
+}
+
+pub fn read_local_nbang_catalog(github_user: &str) -> anyhow::Result<Catalog> {
+    let home_dir: PathBuf = dirs::home_dir().unwrap();
+    let dbang_catalog_json_file = Path::new(&home_dir)
+        .join(".dbang")
+        .join("catalogs/github")
+        .join(github_user)
+        .join("dbang-catalog")
+        .join("dbang-catalog.json");
+    let data = fs::read_to_string(dbang_catalog_json_file).expect("Unable to read dbang-catalog.json");
+    let catalog: Catalog = serde_json::from_str(&data).expect("Unable to parse dbang-catalog.json");
     Ok(catalog)
 }
 
@@ -86,9 +133,28 @@ mod tests {
     }
 
     #[test]
-    fn test_fetch_nbang_catalog() {
-        let catalog = fetch_nbang_catalog("linux-china").unwrap();
-        println!("{:#?}", catalog);
+    fn test_save_nbang_catalog() -> anyhow::Result<()> {
+        let github_user = "linux-china";
+        save_nbang_catalog(github_user)?;
+        if local_nbang_catalog_exists(github_user).is_ok() {
+            let catalog = read_local_nbang_catalog(github_user)?;
+            println!("catalog = {:?}", catalog);
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn test_fetch_remote_nbang_catalog() -> anyhow::Result<()> {
+        let catalog = fetch_remote_nbang_catalog("linux-china")?;
+        println!("catalog = {:?}", catalog);
+        Ok(())
+    }
+
+    #[test]
+    fn test_read_local_dbang_catalog() -> anyhow::Result<()> {
+        let catalog = read_local_nbang_catalog("linux-china")?;
+        println!("catalog = {:?}", catalog);
+        Ok(())
     }
 
     #[test]
