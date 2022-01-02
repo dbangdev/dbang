f8b640351be310035e881168d6050c611524572b
linux_china <libing.chen@gmail.com>
2022-01-01 19:30:10 -0800

    Add custom repo name support

--- src/main.rs
+++ src/main.rs
@@ -32,16 +32,22 @@ fn main() {
         }
         let artifact_full_name = args.value_of("artifact").unwrap();
         dbang_run(artifact_full_name, &artifact_args).unwrap();
+    } else if sub_command == "catalog" {
+
+    } else if sub_command == "deno" {
+        //dbang_version();
+    } else {
+        println!("{}", "Unknown subcommand");
     }
 }
 
 fn dbang_run(artifact_full_name: &str, artifact_args: &[&str]) -> anyhow::Result<()> {
     let artifact_parts: Vec<&str> = artifact_full_name.split("@").collect();
-    let github_user = artifact_parts[1];
+    let repo_name = artifact_parts[1];
     let artifact_name = artifact_parts[0];
     // validate local catalog exists or not
-    if !catalog::Catalog::local_exists(github_user)? {
-        let catalog = catalog::Catalog::fetch_from_github(github_user)?;
+    if !catalog::Catalog::local_exists(repo_name)? {
+        let catalog = catalog::Catalog::fetch_from_github(repo_name)?;
         let catalog_json = serde_json::to_string(&catalog)?;
         println!("Detail of nbang-catalog.json:");
         println!("{}", catalog_json.to_colored_json_auto()?);
@@ -50,15 +56,15 @@ fn dbang_run(artifact_full_name: &str, artifact_args: &[&str]) -> anyhow::Result
         let mut buffer = String::new();
         io::stdin().read_line(&mut buffer)?;
         if buffer.trim().starts_with("y") {
-            catalog::save_nbang_catalog_from_json(github_user, &catalog_json)?;
-            catalog.cache_artifacts(github_user)?;
+            catalog::save_nbang_catalog_from_json(repo_name, &catalog_json)?;
+            catalog.cache_artifacts(repo_name)?;
         } else {
             println!("Abort to accept nbang catalog!");
             return Ok(());
         }
     }
-    let artifact = catalog::Artifact::read_from_local(github_user, artifact_name).unwrap();
-    let script_url = artifact.get_script_http_url(github_user);
+    let artifact = catalog::Artifact::read_from_local(repo_name, artifact_name).unwrap();
+    let script_url = artifact.get_script_http_url(repo_name);
     let permissions: Vec<String> = artifact.get_deno_permissions();
     deno_cli::run(&script_url,
                   artifact_args,
