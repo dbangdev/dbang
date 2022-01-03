9159c5c45841f112bc460b5d93c36d95b2bc6274
linux_china <libing.chen@gmail.com>
2022-01-02 17:42:10 -0800

    implement alias finding

--- src/dbang-shim.rs
+++ src/dbang-shim.rs
@@ -1,8 +1,35 @@
+mod dbang_utils;
+mod aliases;
+
+use std::process::{Command, Stdio};
+
 pub fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    let mut alias = args[0].clone();
+    let mut alias: String = std::env::args().nth(0).unwrap();
     if alias.contains("/") {
         alias = alias.split("/").last().unwrap().to_string();
     }
-    println!("alias name: {}", alias);
+    let script_args = std::env::args().skip(1).collect::<Vec<String>>();
+    if let Some(script_name) = aliases::find_script_name_by_alias(&alias) {
+        dbang_run(&script_name, &script_args).unwrap();
+    } else {
+        println!("{} not found!", alias);
+    }
+}
+
+fn dbang_run(script_full_name: &str, args: &[String]) -> anyhow::Result<()> {
+    Command::new("dbang")
+        .arg("run")
+        .arg(script_full_name)
+        .args(args)
+        .stdin(Stdio::inherit())
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .output()
+        .unwrap();
+    Ok(())
+}
+
+mod tests {
+    #[test]
+    fn test_find_script_name() {}
 }
