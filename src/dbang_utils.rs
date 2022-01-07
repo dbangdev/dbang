2f6cac03223cec70d4bc817c08485e7edf016140
linux_china <libing.chen@gmail.com>
2022-01-07 13:02:35 -0800

    Add github_auth_token()

--- src/dbang_utils.rs
+++ src/dbang_utils.rs
@@ -5,3 +5,24 @@ pub fn dbang_dir() -> PathBuf {
     Path::new(&home_dir)
         .join(".dbang")
 }
+
+pub fn github_auth_token() -> Option<String> {
+    if let Ok(tokens) = std::env::var("DENO_AUTH_TOKENS") {
+        for pair in tokens.split(";") {
+            if pair.contains("@raw.githubusercontent.com") {
+                return pair.split("@").nth(0).map(|s| s.to_string());
+            }
+        }
+    }
+    None
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_github_auth_token() {
+        let token = github_auth_token();
+        println!("{:?}", token.ok_or("not found"));
+    }
+}
