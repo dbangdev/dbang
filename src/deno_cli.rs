08f0cf493e733d228e5bdd3a1d3a09a3e24746ab
linux_china <libing.chen@gmail.com>
2022-01-21 10:57:55 -0800

    Add run_local() to run local dbang-catalog.json

--- src/deno_cli.rs
+++ src/deno_cli.rs
@@ -28,6 +28,31 @@ pub fn run(repo_name: &str, artifact: &Artifact, args: &[&str]) -> anyhow::Resul
     Ok(output)
 }
 
+pub fn run_local(artifact: &Artifact, args: &[&str]) -> anyhow::Result<Output> {
+    let mut command = Command::new(artifact.get_deno_bin_path());
+    command.arg("run").arg("--no-check");
+    if artifact.permissions.is_some() {
+        command.args(artifact.get_deno_permissions());
+    }
+    if artifact.import_map.is_some() {
+        command.arg("--import-map");
+        command.arg(artifact.import_map.as_ref().unwrap());
+    }
+    if let Some(compat) = artifact.compat {
+        if compat {
+            command.arg("--compat");
+        }
+    }
+    let output = command
+        .arg(&artifact.script_ref)
+        .args(args)
+        .stdin(Stdio::inherit())
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .output()?;
+    Ok(output)
+}
+
 pub fn cache(deno_bin_path: &str, script_name: &str) -> anyhow::Result<Output> {
     let output = Command::new(deno_bin_path)
         .arg("cache")
